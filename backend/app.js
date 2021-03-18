0c67fbf2d7ad187e6348cdf12a6daad316b4f335
Жанна Калинкина <62453022+kalinkinajane@users.noreply.github.com>
2021-03-18 16:06:16 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -23,13 +23,11 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const allowedCors = [
-  'localhost:3003',
-  'https://mestokalinkina.students.nomoredomains.rocks',
-];
-app.use(cors({
-  origin: allowedCors,
-}));
+// const allowedCors = [
+//   'localhost:3003',
+//   'https://mestokalinkina.students.nomoredomains.rocks',
+// ];
+app.use(cors());
 
 app.use(requestLogger);
 
